67f553389d2d21aaa3b7353ef81abaff7b466db4
Shoebaccasaurus <113389994+Shoebaccasaurus@users.noreply.github.com>
2023-01-29 13:13:29 -0500

    The Final Countdown
    *fingers crossed*

--- src/com/coderscampus/MSchuster_Assignment2.java
+++ src/com/coderscampus/MSchuster_Assignment2.java
@@ -10,29 +10,28 @@ public class MSchuster_Assignment2 {
 		Scanner scanner = new Scanner(System.in);
 		Random random = new Random();
 		int randomNumber = random.nextInt(100) + 1;
-		int remainingAttempts = 4;
-//		System.out.println(randomNumber);
+		int remainingAttempts = 5;
 		System.out.println("Guess a Number Between 1-100");
-		int playerGuess = scanner.nextInt();
 //		System.out.println(randomNumber);
 		while (remainingAttempts > 0) {
 			remainingAttempts--;
-
+			int playerGuess = scanner.nextInt();
 			if (playerGuess < 1 || playerGuess > 100) {
 				System.out.println("Your guess is not between 1 and 100, please try again");
 				remainingAttempts++;
 
-			}
-			if (playerGuess < randomNumber) {
-				System.out.println("Please Pick a Higher Number");
-			} else if (playerGuess > randomNumber) {
-				System.out.println("Please Pick a Lower Number");
-			} else if (playerGuess == randomNumber) {
-				System.out.println("You Win!");
-				break;
-			}
+			} else {
 
-			playerGuess = scanner.nextInt();
+				if (playerGuess < randomNumber) {
+					System.out.println("Please Pick a Higher Number");
+				} else if (playerGuess > randomNumber) {
+					System.out.println("Please Pick a Lower Number");
+				} else if (playerGuess == randomNumber) {
+					System.out.println("You Win!");
+					break;
+				}
+			}
+	
 			if (remainingAttempts == 0 && playerGuess != randomNumber) {
 				System.out.println("You lose!");
 				System.out.println("The Number to guess was: " + randomNumber);
